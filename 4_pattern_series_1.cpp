d0e3948d88d640d957af6f882a2640d3988e6fc3
Bharatjain14 <bharatashokjain48@gmail.com>
2023-04-28 12:22:14 +0530

    100 dsa code

---
+++ 4_pattern_series_1.cpp
@@ -0,0 +1,34 @@
+//Pattern series 1
+/*
+Output:
+
+1   5
+ 2 4
+  3
+ 2 4
+1   5
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[100][100],n,i,j;
+
+    cin>>n;    
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if((i==j) || (i+j)==n+1)
+            cout<<j;
+
+            else
+            cout<<" ";
+        }       
+        cout<<endl;
+    } 
+    return 0;
+}
